3bcedafaf0ebc2b734d5784b6a589d856170cac8
Daniel Porrey <porrd0bl@prscoad.com>
2020-05-21 21:23:29 -0500

    Added test projects.

---
+++ Src/Mvc.RazorTools.Base.Tests/UnitTests.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+
+namespace Mvc.RazorTools.Base.Tests
+{
+	[TestFixture]
+	public class Tests
+	{
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+		[Test]
+		public void Test1()
+		{
+			// coming soon...
+			Assert.Pass();
+		}
+	}
+}
---
+++ Src/Mvc.RazorTools.FontAwesome.Tests/UnitTests.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+
+namespace Mvc.RazorTools.FontAwesome.Tests
+{
+	[TestFixture]
+	public class Tests
+	{
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+		[Test]
+		public void Test1()
+		{
+			// coming soon...
+			Assert.Pass();
+		}
+	}
+}
